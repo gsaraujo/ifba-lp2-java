6763c2cfb33399d5ad45e81a364d6d2a229d4df2
Gustavo <sasaeucomvoce@hotmail.com>
2025-05-10 20:15:54 -0300

    feat: question 04, first activity

---
+++ src/main/java/org/example/A001Q04.java
@@ -0,0 +1,12 @@
+package org.example;
+/* Q04 - Um programa que declare duas variáveis inteiras e imprima a soma delas.
+* */
+
+public class A001Q04 {
+    public static void main(String[] args) {
+        int primeiroNumero = 14;
+        int segundoNumero = 37;
+
+        System.out.println(primeiroNumero + segundoNumero);
+    }
+}
