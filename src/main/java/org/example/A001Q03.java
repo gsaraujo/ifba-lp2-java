af24f3e323e9ccb199a0e6295cf370eda75b696d
Gustavo <sasaeucomvoce@hotmail.com>
2025-05-10 20:10:50 -0300

    feat: question 3, first activity

---
+++ src/main/java/org/example/A001Q03.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class A001Q03 {
+    public static void main(String[] args) {
+        System.out.print("Olá, mundo");
+        System.out.print("Tudo bem?");//a gente pode ver que o texto fica na mesma linha. Não tem quebra de linha
+    }
+}
