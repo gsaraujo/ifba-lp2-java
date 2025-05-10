1e73576bcda11bd0cbeb142cc2e57981f41fe694
Gustavo <sasaeucomvoce@hotmail.com>
2025-05-10 20:19:27 -0300

    feat: question 05, first activity

---
+++ src/main/java/org/example/A001Q05.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/* Um programa que imprima a ficha de um aluno com nome, idade, turma e instituição de ensino
+* */
+
+public class A001Q05 {
+    public static void main(String[] args) {
+        String nome = "Aluno Exemplar";
+        int idade = 15;
+        String turma = "Turma Exemplar";
+        String instituicao = "IFBA";
+
+        System.out.println("___________________________");
+        System.out.println("Ficha do aluno: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Turma: " + turma);
+        System.out.println("Instituicao: " + instituicao);
+        System.out.println("___________________________");
+
+    }
+}
