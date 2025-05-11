4bb45d40cef80c948aa35d6d88773af79e272393
Gustavo <sasaeucomvoce@hotmail.com>
2025-05-10 21:04:23 -0300

    feat: question 09, first activity

--- src/main/java/org/example/A001Q01.java
+++ src/main/java/org/example/A001Q01.java
@@ -1,5 +1,8 @@
 package org.example;
 
+/* Um programa que imprima Olá, mundo.
+* */
+
 public class A001Q01 {
     public static void main(String[] args) {
         System.out.println("Olá, mundo");
--- src/main/java/org/example/A001Q02.java
+++ src/main/java/org/example/A001Q02.java
@@ -1,5 +1,8 @@
 package org.example;
 
+/* Um programa que imprima Olá, mundo em uma linha e Tudo bem? Em outra linha.
+* */
+
 public class A001Q02 {
     public static void main(String[] args) {
         System.out.println("Olá, mundo");
